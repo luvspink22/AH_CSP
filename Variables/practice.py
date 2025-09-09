66d9e79a41dfd61e2e693df0283b5ee112a88560
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-09-08 19:25:24 -0600

    Added variable.py file

---
+++ Variables/practice.py
@@ -0,0 +1,30 @@
+#name
+my_name= "Ambar"
+print("name:", my_name)
+#number between 1 and 100
+random_number = 6
+print("Number (1-10):", random_number)
+#number between 100 and 1000
+big_number = 300
+print("Number (100-1000):", big_number)
+# breakfast
+breakfast = "Bagel"
+print("Breakfast:", breakfast)
+#favorite Color
+favorite_color = "Pink" 
+print("Favorite color", favorite_color)
+#School_name
+School_name = "UCAS"
+print("School name:", School_name)
+#Year
+year = "2025"
+print("Year", year)
+#Eye color
+eye_color = "Dark Brown"
+print("Eye brown", eye_color)
+#Age 
+age = 15
+print("Age:", age)
+#Favorite subject
+favorite_subject = "Art"
+print("Favorite Subject:", favorite_subject)
