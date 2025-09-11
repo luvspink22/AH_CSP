35d7cdb493d3dff4dc8806dc87b2659d1b34264e
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-09-11 00:12:16 -0600

    Create name_decorator.py

---
+++ Variables/name_decorator.py
@@ -0,0 +1,10 @@
+# AH, Period 7, Name Decorator Assignment
+
+# Hard-coded name
+name = "Ambar"
+
+# Add smiley face decorations
+decorated_name = ":) :) " + name + " :) :)"
+
+# Print the decorated name
+print(decorated_name)
