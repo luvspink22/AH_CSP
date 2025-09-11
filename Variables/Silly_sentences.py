9cf7dbbb631f9c4ea096cec081d8816525a68d43
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-09-11 00:18:04 -0600

    Create Silly_sentences.py

---
+++ Variables/Silly_sentences.py
@@ -0,0 +1,12 @@
+# AH, Period 7, Silly Sentences Assignment
+
+# Ask the user for 3 words
+word1 = input("Enter a Roblox pet: ")
+word2 = input("Enter an adjective: ")
+word3 = input("Enter a funny action: ")
+
+# Create a silly sentence using all 3 words
+sentence = f"My {word2} {word1} glitched out in Roblox and started to {word3} all over the map"
+
+# Print the silly story
+print(sentence)
