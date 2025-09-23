fdf0a77ab748c44294c3d54967acaa08e5d97e98
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-09-22 23:20:38 -0600

    Update update_hello.py

--- update_hello.py
+++ update_hello.py
@@ -1,11 +1,20 @@
 # AH 6th period Update Hello World - python 
+
 def say_hello(name):
-    """Function that takes in a name and says hello"""
+    # say hello to the names
     print(f"Hello, {name}!")
 
-# Call the function with the required names
+# hello to Ambar
 say_hello("Ambar")
+
+# hello to Henriquez
 say_hello("Henriquez")
+
+# hello to Diana
 say_hello("Diana")
+
+# hello to David
 say_hello("David")
+
+# hello to Sofia
 say_hello("Sofia")
