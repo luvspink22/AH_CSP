311f3453f4412180ea0fa5443d33e526e65ba260
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-09-23 23:22:40 -0600

    Create my_family_loop.py

---
+++ my_family_loop.py
@@ -0,0 +1,8 @@
+#  AH period 6 My Family Loop - Python
+
+# My list of cousins and siblings
+family = ["Sofia", "Daniel", "Diana", "David"]
+
+# Saying Hello to my family members
+for person in family:
+    print("Hello, " + person + "!")
