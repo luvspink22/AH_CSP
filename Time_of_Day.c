c1437ae5c7531e1a4a7db5acb105f3d330266e51
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-10-10 19:08:09 -0600

    Create Time_of_Day.c

---
+++ Time_of_Day.c
@@ -0,0 +1,30 @@
+#AH period 6 Time of Day - C
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    // get the current system time
+    time_t t;
+    struct tm *tm_info;
+    time(&t);
+    tm_info = localtime(&t);
+
+    int hour = tm_info->tm_hour;
+
+    // print the right greeting depending on the hour
+    if (hour >= 5 && hour <= 11) {
+        printf("Good Morning!\n");
+    } 
+    else if (hour >= 12 && hour <= 16) {
+        printf("Good Afternoon!\n");
+    } 
+    else if ((hour >= 17 && hour <= 23) || (hour >= 0 && hour <= 4)) {
+        printf("Good Evening!\n");
+    } 
+    else {
+        printf("Invalid hour entered.\n");
+    }
+
+    return 0;
+}
