099fb69e296b63e1a51ee22f2d7f41abfb38a635
luvspink22 <ambar.henriquez@ucas-edu.net>
2025-10-15 22:37:20 -0600

    Update comments in Update_Hello_Update.c

---
+++ Update_Hello_Update.c
@@ -0,0 +1,19 @@
+//AH period 6 Update Hello World - C
+
+#include <stdio.h>
+
+// saying hello to the person
+void say_hello(const char *name) {
+    printf("Hello %s!\n", name);
+}
+
+int main() {
+    // inserting names 
+    say_hello("Ambar");
+    say_hello("Jnayla");
+    say_hello("Bethany");
+    say_hello("Sofia");
+    say_hello("Ashley");
+
+    return 0;
+}
